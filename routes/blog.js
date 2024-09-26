e460781dff770e0272512db84a8efdab77aa76d4
Mohammad-Saif72 <saifansari723693@gmail.com>
2024-09-26 19:15:29 +0530

    completed

--- routes/blog.js
+++ routes/blog.js
@@ -4,10 +4,16 @@ const router = express.Router();
 // controller ko import karo;
 const dummy = require('../controller/dummy');
 const {createComment} = require('../controller/commentController');
+const { createPost, getAllpost } = require('../controller/postController');
+const { likePost, unlikePost } = require('../controller/likeController');
 
 // mapping creat karo;
-router.get('/dummy',dummy);
+// router.get('/dummy',dummy);
 router.post('/comments/create',createComment);
+router.post('/posts/create',createPost);
+router.get('/posts',getAllpost);
+router.post('/likes/like',likePost);
+router.post('/likes/unlike',unlikePost);
 
 // export 
 module.exports = router;
